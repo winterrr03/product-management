fabb7913b2761a3addd312e668414c92d8078a75
winterrr03 <dong.huynhquang21503@gmail.com>
2024-06-09 22:56:24 +0700

    feat: create, edit groupauth

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -3,6 +3,7 @@ const systemConfig = require("../../config/system");
 const dashboardRoutes = require("./dashboard.route");
 const productRoutes = require("./product.route");
 const productCategoryRoutes = require("./product-category.route");
+const roleRoutes = require("./role.route");
 
 module.exports = (app) => {
     const PATH_ADMIN = systemConfig.prefixAdmin;
@@ -12,4 +13,6 @@ module.exports = (app) => {
     app.use(PATH_ADMIN + "/products", productRoutes);
 
     app.use(PATH_ADMIN + "/products-category", productCategoryRoutes);
+
+    app.use(PATH_ADMIN + "/roles", roleRoutes);
 }
