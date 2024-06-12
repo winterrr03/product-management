d4dedd3072c4ed52cd97ba5ce6d1a284c9ac8dd6
winterrr03 <dong.huynhquang21503@gmail.com>
2024-06-12 15:47:03 +0700

    feat: info acc

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -8,6 +8,7 @@ const productCategoryRoutes = require("./product-category.route");
 const roleRoutes = require("./role.route");
 const accountRoutes = require("./account.route");
 const authRoutes = require("./auth.route");
+const myAccountRoutes = require("./my-account.route");
 
 module.exports = (app) => {
     const PATH_ADMIN = systemConfig.prefixAdmin;
@@ -38,4 +39,9 @@ module.exports = (app) => {
         accountRoutes);
 
     app.use(PATH_ADMIN + "/auth", authRoutes);
+
+    app.use(
+        PATH_ADMIN + "/my-account",
+        authMiddleware.requireAuth,
+        myAccountRoutes);
 }
