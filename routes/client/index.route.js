754ecec004226652611626351223874355b6bd9d
winterrr03 <dong.huynhquang21503@gmail.com>
2024-06-17 17:19:34 +0700

    feat: show menu category

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,7 +1,11 @@
-const productRoutes = require("./product.route")
-const homeRoutes = require("./home.route")
+const productRoutes = require("./product.route");
+const homeRoutes = require("./home.route");
+
+const categoryMiddleware = require("../../middlewares/client/category.middleware");
 
 module.exports = (app) => {
+    app.use(categoryMiddleware.category);
+
     app.use("/", homeRoutes);
     
     app.use("/products", productRoutes);
