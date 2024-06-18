645ee7e5fca4db9b6f50c3d176a136547dca2d34
winterrr03 <dong.huynhquang21503@gmail.com>
2024-06-18 22:48:40 +0700

    feat(cart): add product cart

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,18 @@
-console.log("ok");
+// show-alert
+const showAlert = document.querySelector("[show-alert]");
+if(showAlert) {
+  let time = showAlert.getAttribute("data-time");
+  time = parseInt(time);
+
+  // Sau time giây sẽ đóng thông báo
+  setTimeout(() => {
+    showAlert.classList.add("alert-hidden");
+  }, time);
+
+  // Khi click vào nút close-alert sẽ đóng luôn
+  const closeAlert = showAlert.querySelector("[close-alert]");
+  closeAlert.addEventListener("click", () => {
+    showAlert.classList.add("alert-hidden");
+  });
+}
+// End show-alert
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,15 +1,21 @@
 const productRoutes = require("./product.route");
 const homeRoutes = require("./home.route");
 const searchRoutes = require("./search.route");
+const cartRoutes = require("./cart.route");
 
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
+const cartMiddleware = require("../../middlewares/client/cart.middleware");
 
 module.exports = (app) => {
     app.use(categoryMiddleware.category);
+
+    app.use(cartMiddleware.cart);
 
     app.use("/", homeRoutes);
     
     app.use("/products", productRoutes);
 
     app.use("/search", searchRoutes);
+
+    app.use("/cart", cartRoutes);
 }
