2ee86a0cfd16eb16374ed14bb2a97f75a52ebca3
winterrr03 <dong.huynhquang21503@gmail.com>
2024-06-19 21:09:42 +0700

    feat(auth): register

---
+++ validates/client/user.validate.js
@@ -0,0 +1,40 @@
+const User = require("../../models/user.model");
+
+module.exports.registerPost = async (req, res, next) => {
+  if(!req.body.fullName) {
+    req.flash("error", "Vui lòng nhập họ tên!");
+    res.redirect("back");
+    return;
+  }
+
+  if(req.body.fullName.length < 5) {
+    req.flash("error", "Vui lòng nhập ít nhất 5 ký tự!");
+    res.redirect("back");
+    return;
+  }
+
+  if(!req.body.email) {
+    req.flash("error", "Vui lòng nhập email!");
+    res.redirect("back");
+    return;
+  }
+
+  if(!req.body.password) {
+    req.flash("error", "Vui lòng nhập mật khẩu!");
+    res.redirect("back");
+    return;
+  }
+
+  const existEmail = await User.findOne({
+    email: req.body.email,
+    deleted: false
+  });
+
+  if(existEmail) {
+    req.flash("error", "Email đã tồn tại!");
+    res.redirect("back");
+    return;
+  }
+
+  next();
+}
